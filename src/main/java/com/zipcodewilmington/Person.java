2828b7c33fe37afe6a3c1fca933b3f24bdbddb23
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-10-19 15:03:04 -0400

    Initial commit

---
+++ src/main/java/com/zipcodewilmington/Person.java
@@ -0,0 +1,31 @@
+package com.zipcodewilmington;
+
+/**
+ * Created by leon on 1/24/18.
+ */
+public class Person {
+    private final String firstName;
+    private final String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // why is this called a GETTER??
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override // why is Override here??
+    public String toString() {
+        return new StringBuilder()
+                .append("\nMy first name is " + firstName)
+                .append("\nMy last name is " + lastName)
+                .toString();
+    }
+}
---
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -0,0 +1,68 @@
+package com.zipcodewilmington;
+
+/**
+ * Created by leon on 1/24/18.
+ */
+public class PersonHandler {
+    private final Person[] personArray;
+
+    public PersonHandler(Person[] personArray) {
+        this.personArray = personArray;
+    }
+
+    // NOTICE how the comments can be used to step-by-step break down the problem into 
+    // simple lines of code...
+    
+    public String whileLoop() {
+        String result = "";
+        // create a `counter`
+        // while `counter` is less than length of array
+            // begin loop
+
+                // use `counter` to identify the `current Person` in the array
+                // get `string Representation` of `currentPerson`
+                // append `stringRepresentation` to `result` variable
+
+            // end loop
+        return result;
+    }
+
+
+
+    public String forLoop() {
+        String result = "";
+        // identify initial value
+        // identify terminal condition
+        // identify increment
+
+        // use the above clauses to declare for-loop signature
+            // begin loop
+                // use `counter` to identify the `current Person` in the array
+                // get `string Representation` of `currentPerson`
+                // append `stringRepresentation` to `result` variable
+            // end loop
+
+        return result;
+    }
+
+
+
+    public String forEachLoop() {
+        String result = "";
+        // identify array's type
+        // identify array's variable-name
+
+        // use the above discoveries to declare for-each-loop signature
+            // begin loop
+                // get `string Representation` of `currentPerson`
+                // append `stringRepresentation` to `result` variable
+            // end loop
+
+        return result;
+    }
+
+
+    public Person[] getPersonArray() {
+        return personArray;
+    }
+}
